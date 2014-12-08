6415caaf37060d5ea8d69ed3a5616e065c513e0d
rainashastri30 <rainashastri30@gmail.com>
2014-12-07 21:37:06 -0300

    Java Code
    This is just a sample commit. This is not the final code

---
+++ projRfid/src/main/java/projRfid/projRfid/BedController.java
@@ -0,0 +1,107 @@
+package projRfid.projRfid;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BedController {
+
+	@Autowired
+	BedInfo bedInfo;
+
+	@Autowired
+	BedInfoService bedInfoService;
+
+	List<BedInfo> bedInfoList = new ArrayList<BedInfo>();
+
+	@RequestMapping(value = "/addBed", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView addBedInfo() {
+
+		ModelAndView model = new ModelAndView();
+
+		model.setViewName("AddBed");
+		return model;
+	}
+
+	@RequestMapping(value = "/deleteBeds", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView showBeds() {
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("showAllBeds");
+		return model;
+	}
+
+	
+	@RequestMapping(value = "/viewBeds", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView viewBeds() {
+
+		ModelAndView model = new ModelAndView();
+
+		model.setViewName("showAllBeds");
+		return model;
+	}
+
+	// Add a bed
+
+	@RequestMapping(value = "/addBed", method = RequestMethod.POST)
+	@ResponseBody
+	public boolean addBed() {
+
+		boolean status = false;
+		status = bedInfoService.addBed();
+
+		return status;
+	}
+
+	// del a bed
+
+	@RequestMapping(value = "/deleteBed/{bedId}", method = RequestMethod.GET)
+	@ResponseBody
+	public boolean deleteBed(@PathVariable int bedId) {
+
+		boolean status = false;
+		status = bedInfoService.deleteBed(bedId);
+
+		return status;
+	}
+
+	// View all Beds
+	@RequestMapping(value = "/viewAllBeds", method = RequestMethod.GET)
+	@ResponseBody
+	public BedResp bedInfo() {
+
+		BedResp reponseStr = new BedResp();
+		reponseStr = bedInfoService.viewAllBeds();
+		reponseStr.setTotalPages(40);
+		reponseStr.setCurrPage(4);
+		return reponseStr;
+	}
+
+	// Assign a bed
+
+	@RequestMapping(value = "/assignBed/{userId}/{bedId}", method = RequestMethod.GET)
+	@ResponseBody
+	public boolean assignBed(@PathVariable int userId, @PathVariable int bedId) {
+
+		boolean status = false;
+		status = bedInfoService.assignBed(userId, bedId);
+
+		return status;
+	}
+
+}
